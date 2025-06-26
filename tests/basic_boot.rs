d36c835c82344c7187fce84898c1eda70f73ed8b
doqin <personal.azalea@gmail.com>
2025-06-26 15:28:10 +0700

    Add initial implementation of the mai_os kernel with basic boot and serial output functionality

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,29 +5,20 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
 use mai_os::println;
 
 #[unsafe(no_mangle)] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
-    #[cfg(test)]
     test_main();
-    println!("Hello {}", "World!");
-
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     mai_os::test_panic_handler(info);
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
